c147e6d3cee805dd0530544f5c4d0a739051fcb6
guptashashankmca <165798455+guptashashankmca@users.noreply.github.com>
2025-05-20 19:29:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+async function getWeather(){
+	const apiKey = "3581693e48907eebbe85c175f0aa3453";
+	const city = "London";
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+	try {
+		const response = await fetch (url);
+		if(!response.ok) throw new Error `failed to fetch weather data`;
+		const data = await response.json();
+		const condition = data.weather[0].main;
+		document.getElementById("weatherData").innerText=`Current weather in London: ${condition}`;
+	} catch (error) {
+		document.getElementById().innerText = `Error in fetching data`;
+		console.log(error);
+	}
+}
